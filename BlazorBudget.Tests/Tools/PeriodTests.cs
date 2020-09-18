1d55155612a2689a11ceec9c678d29b51a321161
cfloryiv <cfloryiv@rcn.com>
2020-09-18 03:05:03 -0400

    setup for unit tests for period class

---
+++ BlazorBudget.Tests/Tools/PeriodTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BlazorBudget.Tools;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlazorBudget.Tests.Tools
+{
+    [TestClass]
+    public class PeriodTests
+    {
+        [TestMethod]
+        public void TestInvalidPeriod()
+        {
+            var period=new Period("2020-10");
+            Assert.AreEqual(period.PeriodOut, "2020-10");
+        }
+
+        [TestMethod]
+        public void TestPeriodBumpsAtNormalMonth()
+        {
+            var period = new Period("2020-10");
+            period.bumpPeriod();
+            Assert.AreEqual(period.PeriodOut, "2020-11");
+        }
+
+        [TestMethod]
+        public void TestPeriodBumpsAtYearEnd()
+        {
+            var period = new Period("2020-12");
+            period.bumpPeriod();
+            Assert.AreEqual(period.PeriodOut, "2021-01");
+        }
+
+        [TestMethod]
+        public void TestPeriodDoesntBumpBadPeriod()
+        {
+            var period = new Period("junk");
+            period.bumpPeriod();
+            Assert.AreEqual(period.PeriodOut, "junk");
+        }
+    }
+}
