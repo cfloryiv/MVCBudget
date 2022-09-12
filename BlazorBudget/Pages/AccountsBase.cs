fcef1bc51a4d165d23498d5cb5903e7568f4e6c6
cfloryiv <cfloryiv@rcn.com>
2022-09-11 22:01:51 -0400

    reduce size of reports. remove extra windows that are not needed to run

--- BlazorBudget/Pages/AccountsBase.cs
+++ BlazorBudget/Pages/AccountsBase.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-using BlazorBudget.Services;
+﻿using BlazorBudget.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Logging;
 using MVCBudget.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BlazorBudget.Pages
 {
-    public class AccountsBase: ComponentBase
+    public class AccountsBase : ComponentBase
     {
         [Inject]
         public IAccountDataService AccountDataService { get; set; }
@@ -30,12 +29,12 @@ namespace BlazorBudget.Pages
                 accounts = (await AccountDataService.GetAccounts())
                     .OrderBy(a => a.Name)
                     .ToList();
-            } 
-            catch(Exception e)
+            }
+            catch (Exception e)
             {
                 Message = "Something went wrong.";
             }
-            
+
         }
         /*
         public async void AddEmployeeDialog_OnDialogClose()
